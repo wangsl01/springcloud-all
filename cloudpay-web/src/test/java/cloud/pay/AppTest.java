beb1d7fe35f83bd6a5ad8632ef3c340715ddac26
wangsl <wearetheirs@163.com>
2019-03-05 12:22:34 +0800

    参考 作者：vbirdbest 来源：CSDN 原文：https://blog.csdn.net/vbirdbest/article/details/79863883 实现 springboot starter

--- cloudpay-web/src/test/java/cloud/pay/AppTest.java
+++ cloudpay-web/src/test/java/cloud/pay/AppTest.java
@@ -1,20 +1,28 @@
 package cloud.pay;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+//import static org.junit.Assert.assertTrue;
+//
+//import cloud.pay.service.PersonService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple AppWeb.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class AppTest {
+
+//    @Autowired
+//    private PersonService personService;
+//
+//    @Test
+//    public void testHelloWorld() {
+//        personService.sayHello();
+//        System.out.println("========================================");
+//    }
 }
